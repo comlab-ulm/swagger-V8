07ec3c51a017e9dc517cca7d012ba38a8a9ba959
russkiy <russkiy78@gmail.com>
2022-01-28 09:39:22 +0100

    add route

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import React from 'react';
 import './App.css';import SwaggerUI from "swagger-ui-react"
 import "swagger-ui-react/swagger-ui.css"
+import {Route, Switch} from "react-router-dom";
 
 
 function App() {
-    return <SwaggerUI url="./openapi.json" />
+    return (
+        <Switch>
+            <Route path="/swagger-V8/" exact={true}>
+                <SwaggerUI url="./openapi.json" />
+            </Route>
+            <Route path="/swagger-V8/benchmark" exact={true}>
+                <SwaggerUI url="./benchmark.json" />
+            </Route>
+        </Switch>
+    )
 }
 
 export default App;
